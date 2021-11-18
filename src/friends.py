f919289935f3638c8ef8678e25aaeac8c149cd4a
petermordaunt <peetfire@hotmail.com>
2021-11-18 14:39:25 +0000

    First commit

---
+++ src/friends.py
@@ -0,0 +1,15 @@
+
+
+#1
+def get_name(person):
+    return person["name"]
+
+def get_favourite_tv_show(person):
+    return person["favourites"]["tv_show"]
+
+def likes_to_eat(person, food):
+    snacks = person["favourites"]["snacks"]
+    if snacks.index(food) != ValueError:
+        return True
+    return False
+        
